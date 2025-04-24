e273fed6cd767f119dc92d7db2ca0ee742e8929b
OBZIUS <aryankahate007@gmail.com>
2025-04-25 02:01:51 +0530

    Testing

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -4,6 +4,7 @@ import User from "../../assets/images/user.png";
 
 export const NavigationBar = () => {
     return (
+        
         <div className="navigation-bar">
             <img className="logo" alt="Newlogo" src={Logo} />
             <img className="user" alt="Newuser" src={User} />
