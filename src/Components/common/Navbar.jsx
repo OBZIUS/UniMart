0daff4df2ed57821b616d59ce4b407b1478f7310
OBZIUS <aryankahate007@gmail.com>
2025-04-25 01:58:35 +0530

    Testing SSH key

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -4,7 +4,6 @@ import User from "../../assets/images/user.png";
 
 export const NavigationBar = () => {
     return (
-        
         <div className="navigation-bar">
             <img className="logo" alt="Newlogo" src={Logo} />
             <img className="user" alt="Newuser" src={User} />
